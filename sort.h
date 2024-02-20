03910622999002929ae39539d1a2d2e95ea6d9f1
realjohnadex <johnadex60@gmail.com>
2024-02-20 03:12:16 -0800

    Adding header file

--- sort.h
+++ sort.h
@@ -23,6 +23,7 @@ void print_list(const listint_t *list);
 
 /* Declare prototypes for your sorting functions here */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
 
